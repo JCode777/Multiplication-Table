6fb0c4c3f7701267c9f0b381e8eea3de018d216f
J <jfelton8899@gmail.com>
2021-03-02 17:13:46 -0500

    Delegation instead of multiple event listeners

--- index.js
+++ index.js
@@ -35,51 +35,42 @@ let buttonNumberTen = document.getElementById('ten');
 let buttonNumberEleven = document.getElementById('eleven');
 let buttonNumberTwelve = document.getElementById('twelve');
 
-buttonNumberOne.addEventListener('click', function() {
-    outputTable(1);
-    changeColor('#801515');
-});
-buttonNumberTwo.addEventListener('click', function() {
-    outputTable(2);
-    changeColor('#0D4D4D');
-});
-buttonNumberThree.addEventListener('click', function() {
-    outputTable(3);
-    changeColor('#116611'); 
-});
-buttonNumberFour.addEventListener('click', function() {
-    outputTable(4);
-    changeColor('#804515'); 
-});
-buttonNumberFive.addEventListener('click', function() {
-    outputTable(5);
-    changeColor('#591717'); 
-});
-buttonNumberSix.addEventListener('click', function() {
-    outputTable(6);
-    changeColor('#0E3636'); 
-});
-buttonNumberSeven.addEventListener('click', function() {
-    outputTable(7);
-    changeColor('#124812'); 
-});
-buttonNumberEight.addEventListener('click', function() {
-    outputTable(8);
-    changeColor('#593517'); 
-});
-buttonNumberNine.addEventListener('click', function() {
-    outputTable(9);
-    changeColor('#550000'); 
-});
-buttonNumberTen.addEventListener('click', function() {
-    outputTable(10);
-    changeColor('#003333'); 
-});
-buttonNumberEleven.addEventListener('click', function() {
-    outputTable(11);
-    changeColor('#004400'); 
-});
-buttonNumberTwelve.addEventListener('click', function() {
-    outputTable(12);
-    changeColor('#552700'); 
+document.getElementById('buttonBox').addEventListener('click', event => {
+    if(event.target.id === 'one') {
+        outputTable(1);
+        changeColor('#801515');
+    } else if(event.target.id === 'two') {
+        outputTable(2);
+        changeColor('#0D4D4D');
+    } else if(event.target.id === 'three') {
+        outputTable(3);
+        changeColor('#116611'); 
+    } else if(event.target.id === 'four') {
+        outputTable(4);
+        changeColor('#804515'); 
+    } else if(event.target.id === 'five') {
+        outputTable(5);
+        changeColor('#591717'); 
+    } else if(event.target.id === 'six') {
+        outputTable(6);
+        changeColor('#0E3636'); 
+    } else if(event.target.id === 'seven') {
+        outputTable(7);
+        changeColor('#124812'); 
+    } else if(event.target.id === 'eight') {
+        outputTable(8);
+        changeColor('#593517'); 
+    } else if(event.target.id === 'nine') {
+        outputTable(9);
+        changeColor('#550000'); 
+    } else if(event.target.id === 'ten') {
+        outputTable(10);
+        changeColor('#003333'); 
+    } else if(event.target.id === 'eleven') {
+        outputTable(11);
+        changeColor('#004400'); 
+    } else if(event.target.id === 'twelve') {
+        outputTable(12);
+        changeColor('#552700'); 
+    }
 });
